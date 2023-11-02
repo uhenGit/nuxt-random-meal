d012960ead0a037334c4d536ccd4f31f24a5aa53
uhenGit <uhen.dn@gmail.com>
2023-11-02 17:13:28 +0200

    WIP - fix update favorite list on every call

--- utils/index.js
+++ utils/index.js
@@ -1,17 +1,21 @@
-const savedMeals = process.client && localStorage.getItem('favoriteMeals')
-	? JSON.parse(favoriteMeals)
-	: [];
+const savedMeals = () => {
+	const favoriteMeals = process.client && localStorage.getItem('favoriteMeals');
+
+	return favoriteMeals
+		? JSON.parse(favoriteMeals)
+		: []
+};
 	
 export const addToFavorites = (meal) => {
-	const updatedFavorites = [ ...savedMeals, meal ];
+	const updatedFavorites = [ ...savedMeals(), meal ];
 	localStorage.setItem('favoriteMeals', JSON.stringify(updatedFavorites));
 }
 
 export const removeFromFavorites = (mealId) => {
-	const updatedFavorites = savedMeals.filter(({ idMeal }) => idMeal !== mealId);
+	const updatedFavorites = savedMeals().filter(({ idMeal }) => idMeal !== mealId);
 	localStorage.setItem('favoriteMeals', JSON.stringify(updatedFavorites));
 }
 
 export const isMealInFavorites = (mealId) => {
-	return !!savedMeals.find(({ idMeal }) => idMeal === mealId);
+	return !!savedMeals().find(({ idMeal }) => idMeal === mealId);
 }
